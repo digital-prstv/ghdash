4a339e9c2bf6707dd5a5efbda4a4831a031c5c8b
Jeremiah Russell <jerry@jrussell.ie>
2022-11-20 14:28:16 +0000

    feat(cli): set alternate config
    - config setting with short and long flags.
    - update tests to pass

--- src/cli.rs
+++ src/cli.rs
@@ -2,4 +2,8 @@ use clap::Parser;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-pub struct GhDashCli {}
+pub struct GhDashCli {
+    /// alternate configuration file
+    #[arg(short, long)]
+    config: String,
+}
